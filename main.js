13be2ab0c471bf4a4ef2c083a8f0a58e0895bcf4
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-18 21:22:06 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,38 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-const like_glyphs = document.querySelectorAll('.like-glyph')
-const modal = document.getElementById('modal')
-const modalMessage = document.getElementById('modal-message')
-let currentLike
+document.addEventListener("DOMContentLoaded", () => {
 
+});
+const hearts = document.querySelectorAll(".like-glyph");
 
-const updateLike = like => {
-  if (like.innerHTML === EMPTY_HEART) {
-    like.innerHTML = FULL_HEART
-    like.className = 'activated-heart'
-  } else {
-    like.innerHTML = EMPTY_HEART
-    like.className = ''
-  }
-}
-
-like_glyphs.forEach(like => {
-  like.addEventListener('click', (e) => {
-    currentLike = e.target
-    mimicServerCall()
-      .then((resp) => {
-        updateLike(currentLike)
-      })
-      .catch((err) => {
-        modalMessage.innerHTML = err
-        modal.className = ''
-        setTimeout(() => {
-          modal.className = 'hidden'
-        }, 5000)
-      })
-  })
-})
+hearts.forEach(heart => {
+  heart.addEventListener("click", (event) => {
+     event.preventDefault();
+     mimicServerCall()
+     .then(data => {
+       if(heart.innerText == EMPTY_HEART) {
+         heart.innerText = FULL_HEART;
+       } else {
+         heart.innerText = EMPTY_HEART;
+       }
+     })
+     .catch(error => {
+       document.getElementById("modal").removeAttribute("class", "hidden");
+       document.getElementById("modal-message").innerText += error.message;
+       setTimeout(() => {
+         document.getElementById("modal").setAttribute("class", "hidden");
+       }, 5000);
+     });
+   });
+});
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
